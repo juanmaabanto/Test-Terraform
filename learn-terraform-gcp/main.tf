e659849257deb75ca4d6d56da9ad47cb73a6cb85
Juan Manuel Abanto <jmanuelabanto@gmail.com>
2024-09-27 11:19:41 -0500

    added resource for gcp

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -14,3 +14,22 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+
+    }
+  }
+}
